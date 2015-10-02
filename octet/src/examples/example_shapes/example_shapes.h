9f74e28ab2741bb0714508fa2d2be3c8c7694a11
Jeyp <jp.evette1@gmail.com>
2015-10-02 15:31:42 +0100

    Spring constraint working

--- octet/src/examples/example_shapes/example_shapes.h
+++ octet/src/examples/example_shapes/example_shapes.h
@@ -23,22 +23,25 @@ namespace octet {
 		app_scene = new visual_scene();
 		world = app_scene->get_world();
 		app_scene->create_default_camera_and_lights();
-		app_scene->get_camera_instance(0)->get_node()->translate(vec3(0, 4, 0));
+		app_scene->get_camera_instance(0)->get_node()->translate(vec3(0, 4, 10));
 
 		btRigidBody* rigid_body = NULL;
 		btRigidBody* rigid_body2 = NULL;
+		btRigidBody* rigid_body3 = NULL;
+		btRigidBody* rigid_body4 = NULL;
+		btRigidBody* rigid_body5 = NULL;
 		material *red = new material(vec4(1, 0, 0, 1));
 		material *green = new material(vec4(0, 1, 0, 1));
 		material *blue = new material(vec4(0, 0, 1, 1));
 
 		mat4t mat;
 		mat.loadIdentity();
-		mat.translate(-3, 6, 0);
+		mat.translate(-13, 6, 0);
 		rigid_body = app_scene->createNewObjectWithRigidBody(mat, blue, new mesh_sphere(vec3(2, 2, 2), 2), 1.0f, true);
 
 
 		mat.loadIdentity();
-		mat.translate(0, 15, 0);
+		mat.translate(-10, 15, 0);
 		rigid_body2 = app_scene->createNewObjectWithRigidBody(mat, blue, new mesh_sphere(vec3(2,2,2),2),1.0f, true);
 
 		btHingeConstraint* hinge = new btHingeConstraint(*rigid_body, *rigid_body2, btVector3(-3, 6, 0), btVector3(0, 15, 0), btVector3(1, 0, 0), btVector3(1, 0, 0));
@@ -46,12 +49,55 @@ namespace octet {
 		world->addConstraint(hinge, true);
 
 
+		mat.loadIdentity();
+		mat.translate(8, 0, 5);
+		rigid_body3 = app_scene->createNewObjectWithRigidBody(mat, blue, new mesh_box(vec3(2, 2, 2)), 1.0f, true);
+
+
+
+		mat.loadIdentity();
+		mat.translate(7, 5, 5);
+		rigid_body4 = app_scene->createNewObjectWithRigidBody(mat, blue, new mesh_box(vec3(2, 2, 2)), 1.0f, true);
+
+
+		mat.loadIdentity();
+		mat.translate(13, 0, 5);
+		rigid_body5 = app_scene->createNewObjectWithRigidBody(mat, blue, new mesh_box(vec3(2, 2, 2)), 51.0f, true);
+
+
+		btHingeConstraint* hinge2 = new btHingeConstraint(*rigid_body3, *rigid_body5, btVector3(-3, 0, 5), btVector3(-3, 0, 10), btVector3(0, 1, 0), btVector3(0, 1, 0));
+		hinge2->setLimit(0, 0.5f);
+		world->addConstraint(hinge2, true);
+
+
+		btTransform frameInA, frameInB;
+		frameInA = btTransform::getIdentity();
+		frameInA.setOrigin(btVector3(btScalar(0.0f), btScalar(0.0f), btScalar(0.0f)));
+		frameInB = btTransform::getIdentity();
+		frameInB.setOrigin(btVector3(btScalar(0.0f), btScalar(-5.0f), btScalar(0.0f)));
+		btGeneric6DofSpringConstraint* spring = new btGeneric6DofSpringConstraint(*rigid_body3, *rigid_body4, frameInA, frameInB, true);
+
+		spring->setLinearUpperLimit(btVector3(5., 0., 0.));
+		spring->setLinearLowerLimit(btVector3(-5., 0., 0.));
+
+		spring->setAngularLowerLimit(btVector3(0.f, 0.f, -1.5f));
+		spring->setAngularUpperLimit(btVector3(0.f, 0.f, 1.5f));
+
+		world->addConstraint(spring, true);
+		spring->setDbgDrawSize(btScalar(5.f));
+
+		spring->enableSpring(0, true);
+		spring->setStiffness(0, 19.478f);
+		spring->setDamping(0, 0.3f);
+		spring->setEquilibriumPoint();
+
 	  //app_scene->add_shape(mat, new mesh_box(vec3(2, 2, 2)), red, true);
 	  //scene_node* box = app_scene->get_mesh_instance(1)->get_scene_node();
 
 
       mat.loadIdentity();
-      mat.translate( 3, 6, 0);
+      mat.translate( 2, 0, -1.0f);
+	  mat.rotate(90, 0, 1, 0);
       app_scene->add_shape(mat, new mesh_cylinder(zcylinder(vec3(0, 0, 0), 2, 4)), red, true);
 
       // ground
