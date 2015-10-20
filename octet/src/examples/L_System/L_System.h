35803ade861db93170ec8ddc8faaee512de03bc3
Jeyp <jp.evette1@gmail.com>
2015-10-20 17:12:46 +0100

    fixed scaling issue by increasing far plane

--- octet/src/examples/L_System/L_System.h
+++ octet/src/examples/L_System/L_System.h
@@ -13,6 +13,9 @@ namespace octet {
 	int currentMaterial;
 
 	std::vector<scene_node*> listOfLines;
+	std::vector<vec3> savedPointStack;
+
+	const float HALFSIZE = 1.0f;
   public:
     /// this is called when we construct the class before everything is initialised.
     L_System(int argc, char **argv) : app(argc, argv) {
@@ -22,6 +25,7 @@ namespace octet {
     void app_init() {
       app_scene =  new visual_scene();
       app_scene->create_default_camera_and_lights();
+	  app_scene->get_camera_instance(0)->set_far_plane(1000.0f);
 
 	  currentMaterial = 0;
 	  material *red = new material(vec4(1, 0, 0, 1));
@@ -29,6 +33,7 @@ namespace octet {
 	  material *blue = new material(vec4(0, 0, 1, 1));
 	  material *white = new material(vec4(1, 1, 1, 1));
 	  material *black = new material(vec4(0, 0, 0, 1));
+	  savedPointStack = std::vector<vec3>();
 
 
 
@@ -41,47 +46,72 @@ namespace octet {
 	  listOfMaterials.push_back(black);
 
 	  vec3 nextPoint = drawLine(vec3(0),0.0f);
+
+	  for (int i = 0; i < 3; i ++){
 	  nextPoint = drawLine(nextPoint, 0.0f);
 	  nextPoint = drawLine(nextPoint, 0.0f);
 	  nextPoint = drawLine(nextPoint, 0.0f);
+	  rememberPoint(nextPoint);
+	  nextPoint = drawLine(nextPoint, -15.0f);
+	  nextPoint = retrievePoint();
+	  nextPoint = drawLine(nextPoint, 15.0f);
+	  rememberPoint(nextPoint);
 	  nextPoint = drawLine(nextPoint, 0.0f);
 	  nextPoint = drawLine(nextPoint, 45.0f);
 	  nextPoint = drawLine(nextPoint, 22.0f);
 	  nextPoint = drawLine(nextPoint, -15.0f);
+	  rememberPoint(nextPoint);
 	  nextPoint = drawLine(nextPoint, 25.0f);
 	  nextPoint = drawLine(nextPoint, -315.0f);
 	  nextPoint = drawLine(nextPoint, 45.0f);
 	  nextPoint = drawLine(nextPoint, -435.0f);
+	  nextPoint = retrievePoint();
 	  nextPoint = drawLine(nextPoint, 4445.0f);
 	  nextPoint = drawLine(nextPoint, -451.0f);
+	  nextPoint = retrievePoint();
+	  nextPoint = drawLine(nextPoint, 4445.0f);
+	  nextPoint = drawLine(nextPoint, -451.0f);
+	  }
     }
+
+	vec3 retrievePoint()
+	{
+		vec3 point = savedPointStack[savedPointStack.size() - 1];
+		savedPointStack.pop_back();
+		savedPointStack.shrink_to_fit();
+		return point;
+	}
+
+	void rememberPoint(vec3 point)
+	{
+		savedPointStack.push_back(point);
+	}
 
 	vec3 drawLine(vec3 startingPoint, float angle = 0.0f)
 	{
 		material * color = listOfMaterials[currentMaterial];
 		currentMaterial = (currentMaterial + 1) % listOfMaterials.size();
-		float halfSize = 1.0f;
 		vec3 midPoint, endPoint;
 		if (angle == 0.0f)
 		{
 			midPoint = startingPoint;
-			midPoint.y() = midPoint.y() + halfSize;
+			midPoint.y() = midPoint.y() + HALFSIZE;
 			endPoint = startingPoint;
-			endPoint.y() = endPoint.y() + 2.0f*halfSize;
+			endPoint.y() = endPoint.y() + 2.0f*HALFSIZE;
 		}
 		else
 		{
 			midPoint = startingPoint;
-			midPoint.x() = midPoint.x() + halfSize*cos((angle + 90) * CL_M_PI / 180);
-			midPoint.y() = midPoint.y() + halfSize*sin((angle + 90)* CL_M_PI / 180);
+			midPoint.x() = midPoint.x() + HALFSIZE *cos((angle + 90) * CL_M_PI / 180);
+			midPoint.y() = midPoint.y() + HALFSIZE *sin((angle + 90)* CL_M_PI / 180);
 			endPoint = startingPoint;
-			endPoint.x() = endPoint.x() + 2.0f*halfSize*cos((angle + 90) * CL_M_PI / 180);
-			endPoint.y() = endPoint.y() + 2.0f*halfSize*sin((angle + 90) * CL_M_PI / 180);
+			endPoint.x() = endPoint.x() + 2.0f*HALFSIZE *cos((angle + 90) * CL_M_PI / 180);
+			endPoint.y() = endPoint.y() + 2.0f*HALFSIZE *sin((angle + 90) * CL_M_PI / 180);
 		}
 		mat4t mat = mat4t();
 		mat.loadIdentity();
 		mat.rotate(90.0f, 1, 0, 0);
-		mesh_cylinder *line = new mesh_cylinder(zcylinder(vec3(0), 0.1f, halfSize), mat);
+		mesh_cylinder *line = new mesh_cylinder(zcylinder(vec3(0), 0.1f, HALFSIZE), mat);
 		scene_node *node = new scene_node();
 		app_scene->add_child(node);
 		app_scene->add_mesh_instance(new mesh_instance(node, line, color));
@@ -89,13 +119,20 @@ namespace octet {
 		listOfLines.push_back(node);
 		node->rotate(angle, vec3(0, 0, 1));
 
+
+
 		return endPoint;
 	}
 
 	void checkCamera(int vx, int vy)
 	{
+		float margin = HALFSIZE;
 		if (listOfLines.size() == 0) return;
-		vec3 linePos = listOfLines[listOfLines.size() - 1]->get_position();
+
+		float highestY = 0;
+		for (int i = 0; i < listOfLines.size(); i++)
+			if (listOfLines[i]->get_position().y() > highestY)
+				highestY = listOfLines[i]->get_position().y();
 
 		vec2 fov = app_scene->get_camera_instance(0)->getFov();
 		vec3 cameraPos = app_scene->get_camera_instance(0)->get_node()->get_position();
@@ -107,8 +144,13 @@ namespace octet {
 		//TOA
 
 
-		if (linePos.y() < -lengthOppY || linePos.y() > lengthOppY)
-			app_scene->get_camera_instance(0)->get_node()->translate(vec3(0,0,5.0f));
+		if (highestY - margin   < -lengthOppY || highestY + margin   > lengthOppY)
+		{
+			app_scene->get_camera_instance(0)->get_node()->translate(vec3(0, 0, 5.0f));
+
+			for (int i = 0; i < listOfLines.size();i++)
+				listOfLines[i]->scale(vec3(1.1f, 1, 1.1f));
+		}
 	}
 
     /// this is called to draw the world
